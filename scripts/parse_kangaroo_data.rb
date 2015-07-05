731e21522d915c2321bb0a14846b988adb7de22a
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-07-05 12:20:10 +1000

    Modified data processing script to output to csv for improt into excel and graphing

--- scripts/parse_kangaroo_data.rb
+++ scripts/parse_kangaroo_data.rb
@@ -3,6 +3,9 @@ require 'date'
 
 loc_i = [18,19]
 date_i = 30
+
+output_csv = ""
+
 CSV.foreach(ARGV[0]) do |row|
     loc = row.values_at(*loc_i).map(&:to_f)
     
@@ -17,5 +20,8 @@ CSV.foreach(ARGV[0]) do |row|
         output = loc << d.to_s
         print output
         puts ","
+	output_csv << output.to_csv
     end
 end
+
+File.write('output_kangaroo_canberra.csv', output_csv)
