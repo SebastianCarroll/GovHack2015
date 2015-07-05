855ad2db8a42dab5299af2f4957c45cf73a933d1
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-07-05 12:15:40 +1000

    Tidy code

--- scripts/parse_kangaroo_data.rb
+++ scripts/parse_kangaroo_data.rb
@@ -5,7 +5,11 @@ loc_i = [18,19]
 date_i = 30
 CSV.foreach(ARGV[0]) do |row|
     loc = row.values_at(*loc_i).map(&:to_f)
+    
+    # filter data from jervis bay
     next if loc[1] > 150
+    
+    # Parse the date
     begin
         d = Date.parse(row[date_i])
     rescue
